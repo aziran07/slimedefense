a9d2093013555ee00ef2fca84d4f6a9e8bd22333
kasuobush <rlawodnr1203@gmail.com>
2024-10-28 22:43:45 +0900

    fix some behaviour

--- Assets/Character/SlimeControllers/SlimeController.cs
+++ Assets/Character/SlimeControllers/SlimeController.cs
@@ -4,21 +4,26 @@ using UnityEngine;
 
 public class SlimeController : MonoBehaviour
 {
-    private float _moveInput;
-    private bool _jumpInput;
-    private bool _isJumping;
     public float moveForce;
     public float jumpForce;
     public float maxMoveSpeed;
     public Rigidbody2D slimeRigidbody;
     public Collider2D slimeCollider;
-    private void Move()
+    public Transform groundDetector;
+    
+    
+    public bool IsJumping =>
+        !Physics2D.Raycast(groundDetector.position, Vector2.down, 0.1f, LayerMask.GetMask("Platform"));
+    
+    private void Move(float moveInput)
     {
-        slimeRigidbody.AddForce(Vector2.right * (_moveInput * moveForce), ForceMode2D.Force);
-        if (slimeRigidbody.velocity.x > maxMoveSpeed)
-            slimeRigidbody.velocity = new Vector2(maxMoveSpeed, slimeRigidbody.velocity.y);
-        else if (slimeRigidbody.velocity.x < -maxMoveSpeed)
-            slimeRigidbody.velocity = new Vector2(-maxMoveSpeed, slimeRigidbody.velocity.y);
+        slimeRigidbody.AddForce(Vector2.right * (moveInput * moveForce), ForceMode2D.Impulse);
+        var velocity = slimeRigidbody.velocity;
+        //Debug.Log($"velocity: {velocity}");
+        if (velocity.x > maxMoveSpeed)
+            slimeRigidbody.velocity = new Vector2(maxMoveSpeed, velocity.y);
+        if (velocity.x < -maxMoveSpeed)
+            slimeRigidbody.velocity = new Vector2(-maxMoveSpeed, velocity.y);
     }
 
     private void Jump()
@@ -29,19 +34,23 @@ public class SlimeController : MonoBehaviour
     {
         slimeRigidbody = GetComponent<Rigidbody2D>();
         slimeCollider = GetComponent<Collider2D>();
+        groundDetector = transform.Find("GroundDetector");
     }
 
     private void FixedUpdate()
-    
     {
-        Move();
-        _isJumping = !Physics2D.Raycast(transform.position, Vector2.down, 1f, LayerMask.GetMask("Platform"));
-        if (_jumpInput && !_isJumping) Jump();
+        // 키 안눌러도 moveInput 값은 바로 0이 되는게 아니고 서서히 줄어든다
+        // 그래서 방향키에서 손 뗐는데 미끄러지듯이 멈추는 문제가 있어서 손에서 키 때면 moveInput을 0으로 함
+        var isHorizontalPressed = Input.GetButton("Horizontal");
+        var moveInput = isHorizontalPressed ? Input.GetAxis("Horizontal") : 0;
+        Move(moveInput);
+
+        var isJumpPressed = Input.GetButton("Jump");
+        if (isJumpPressed && !IsJumping)
+            Jump();
     }
 
     private void Update()
     {
-        _moveInput = Input.GetAxis("Horizontal");
-        _jumpInput = Input.GetButton("Jump");
     }
 }
